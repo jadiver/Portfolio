8c07f28cb043696f1008d3e73261dc77efb7554b
jadiver <javierdv180@gmail.com>
2025-02-19 18:46:52 +0100

    commit 12

--- Web/assets/js/main.js
+++ Web/assets/js/main.js
@@ -1,38 +1,69 @@
 /* =====================================================
    Resume section tabs and tab contents
 ===================================================== */
-const resumeTabs = document.querySelector(".resume-tabs");
-const resumePortfolioTabBtns = resumeTabs.querySelectorAll(".tab-btn");
-const resumeTabContents = document.querySelectorAll(".resume-tab-content");
+// const resumeTabs = document.querySelector(".resume-tabs");
+// const resumePortfolioTabBtns = resumeTabs.querySelectorAll(".tab-btn");
+// const resumeTabContents = document.querySelectorAll(".resume-tab-content");
 
-var resumeTabNav = function(resumeTabClick){
-   resumeTabContents.forEach((resumeTabContent) => {
-      resumeTabContent.style.display = "none";
-      resumeTabContent.classList.remove("active");
-   });
+// var resumeTabNav = function(resumeTabClick){
+//    resumeTabContents.forEach((resumeTabContent) => {
+//       resumeTabContent.style.display = "none";
+//       resumeTabContent.classList.remove("active");
+//    });
 
-   resumePortfolioTabBtns.forEach((resumePortfolioTabBtn) => {
-      resumePortfolioTabBtn.classList.remove("active");
-   });
+//    resumePortfolioTabBtns.forEach((resumePortfolioTabBtn) => {
+//       resumePortfolioTabBtn.classList.remove("active");
+//    });
 
-   resumeTabContents[resumeTabClick].style.display = "flex";
+//    resumeTabContents[resumeTabClick].style.display = "flex";
 
-   setTimeout(() => {
-      resumeTabContents[resumeTabClick].classList.add("active");
-   }, 100);
+//    setTimeout(() => {
+//       resumeTabContents[resumeTabClick].classList.add("active");
+//    }, 100);
    
-   resumePortfolioTabBtns[resumeTabClick].classList.add("active");
-}
+//    resumePortfolioTabBtns[resumeTabClick].classList.add("active");
+// }
 
-resumePortfolioTabBtns.forEach((resumePortfolioTabBtn, i) => {
-   resumePortfolioTabBtn.addEventListener("click", () => {
-      resumeTabNav(i);
-   });
-});
+// resumePortfolioTabBtns.forEach((resumePortfolioTabBtn, i) => {
+//    resumePortfolioTabBtn.addEventListener("click", () => {
+//       resumeTabNav(i);
+//    });
+// });
 /* =====================================================
    Service modal open/close function
 ===================================================== */
+const serviceCardWithModals = document.querySelectorAll(".service-container .card-with-modal");
 
+serviceCardWithModals.forEach((serviceCardWithModal) => {
+   const serviceCard = serviceCardWithModal.querySelector(".service-card");
+   const serviceBackDrop = serviceCardWithModal.querySelector(".service-modal-backdrop");
+   const serviceModal = serviceCardWithModal.querySelector(".service-modal");
+   const modalCloseBtn = serviceCardWithModal.querySelector(".modal-close-btn");
+   
+
+   serviceCard.addEventListener("click", () => {
+      serviceBackDrop.style.display = "flex";
+
+      setTimeout(() => {
+         serviceBackDrop.classList.add("active");
+      }, 100);
+
+      setTimeout(() => {
+         serviceModal.classList.add("active");
+      }, 300);
+   });
+
+   modalCloseBtn.addEventListener("click", () => {
+      setTimeout(() => {
+         serviceBackDrop.style.display = "none";
+      }, 500);
+
+      setTimeout(() => {
+         serviceBackDrop.classList.remove("active");
+         serviceModal.classList.remove("active");
+      }, 100);   
+   });
+});
 /* =====================================================
    Portfolio modals, tabs and cards
 ===================================================== */
